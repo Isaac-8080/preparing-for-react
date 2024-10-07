8a78a6285d75dce9b1c0f6b8cbdf9baea5420bd1
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-10-07 01:08:06 +0000

    objects practice

--- object.js
+++ object.js
@@ -77,3 +77,38 @@ students.forEach(student => {
   console.log(`${student.name} : ${student.grade}`);
 
 });
+
+
+// Objective:
+// The goal of this assignment is to reinforce your understanding of JavaScript basics, including object, data types, loops, conditionals.
+// Instructions:
+// 1. Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books.
+let library = [ 
+  {
+    author: 'Bill Gates',
+    title: 'The Road Ahead',
+    readingStatus: true
+  },
+  {
+    author: 'Steve Jobs',
+    title: 'Walter Isaacson',
+    readingStatus: true
+  },
+  {
+    author: 'Suzanne Collins',
+    title:  'Mockingjay: The Final Book of The Hunger Games', 
+    readingStatus: false
+  }
+];
+
+for (let i = 0; i < library.length; i++) {
+  const book = library[i];
+  
+  if (book.readingStatus === true) {
+    book.readingStatus = 'read';
+  } else {
+    book.readingStatus = 'not read';
+  }
+
+  console.log(`Title: ${book.title} \nAuthor: ${book.author} \nReading Status: ${book.readingStatus}`);
+}
